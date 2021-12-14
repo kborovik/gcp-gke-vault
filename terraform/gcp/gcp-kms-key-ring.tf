d595d3509b3ee181ad5f35c1aa0083e2b465ef0f
Konstantin Borovik <kborovik@paypal.com>
2021-12-14 12:18:03 -0500

    enable gke database encryption

--- terraform/gcp/gcp-kms-key-ring.tf
+++ terraform/gcp/gcp-kms-key-ring.tf
@@ -10,3 +10,37 @@ resource "google_kms_key_ring" "us_central1" {
   name     = "us-central1"
   location = "us-central1"
 }
+
+/*
+
+KMS Key to encrypt GKE database
+
+*/
+resource "google_kms_crypto_key" "gke_app_encryption" {
+  name     = "gke-app-encryption"
+  purpose  = "ENCRYPT_DECRYPT"
+  key_ring = google_kms_key_ring.us_central1.id
+
+  version_template {
+    algorithm        = "GOOGLE_SYMMETRIC_ENCRYPTION"
+    protection_level = "HSM"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+/*
+
+Grant cryptoOperator role to GKE default SA
+
+*/
+resource "google_kms_crypto_key_iam_binding" "gke_app_encryption" {
+  crypto_key_id = google_kms_crypto_key.gke_app_encryption.id
+  role          = "roles/cloudkms.cryptoOperator"
+  members = [
+    "serviceAccount:service-${local.project_number}@container-engine-robot.iam.gserviceaccount.com"
+  ]
+}
+
